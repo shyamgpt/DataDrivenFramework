727c4105a5978a40f3a4da539e8661bb413abd55
unknown <shyamgupta1.nmims@gmail.com>
2024-04-10 00:40:44 +0530

    d

--- src/test/java/com/w2a/testcase/AddCustomerTest.java
+++ src/test/java/com/w2a/testcase/AddCustomerTest.java
@@ -1,5 +1,18 @@
 package com.w2a.testcase;
 
 public class AddCustomerTest {
+	
+	
+	@Test(dataProvider="getData")
+	public void addCustomer() {
+		
+		
+		
+	}
+	
+	
+	public Object[][] getData(){
+		
+	}
 
 }
